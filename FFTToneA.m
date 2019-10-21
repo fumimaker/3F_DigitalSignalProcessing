e5926942e11c842d00dd5502fd604ddda1bd23d4
fumi <fumi01200924@gmail.com>
2019-10-21 18:58:01 +0900

    Update FFTToneA.m

--- FFTToneA.m
+++ FFTToneA.m
@@ -1,13 +1,16 @@
 
-%%  fft of tone A.m
+%%  fft of tone A,Cs,E
 %
-%   genrate a 440Hz tone
+%1週間色々調べたり考えてみましたが、サンプリング周波数が何か影響を及ぼしているのではないかと考えら
+%れます。倍音などによる効果により一部の音が強く聞こえるためこうなとも考えられますが、
+%そうであるかはわかりませんでした。他の音でも実験しましたが同様の結果となりました。
+%現実の世界でも起こり得るんでしょうか。エネルギーの総和が正しければ現実でも起こりそうではあります...
 Fs = 8000; % sampling rate
 A = 440; %tone frequency
+sep = power(2, 1/12);
 Cs= A*power(sep, 4); % tone C#
 E = Cs*power(sep, 3);  % tone E
 t = (0:1/Fs:1-1/Fs);
-  % we reduce the amplitude by 0.5 to avoid distotion.
 y = 0.5*sin(2*pi*(A)*t+pi/4) + 0.5*sin(2*pi*(Cs)*t+pi/4) + 0.5*sin(2*pi*(E)*t+pi/4);
 sound(y, Fs);
 fy = fft(y);
