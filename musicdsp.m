f5de13a4183ca2d700fbec3119619941236cdb56
fumi <fumi01200924@gmail.com>
2019-11-05 16:36:00 +0900

    Update musicdsp.m

--- musicdsp.m
+++ musicdsp.m
@@ -1,16 +1,17 @@
 frameLength = 4096;
-N = 100; % filter tap数
-Fp = 10e3; %10kHz passband
-Qp = 4e3; %4kHz passband
-Rp = 0.00057565; % パスバンドリップル
-Rst = 1e-4; % stopバンド抑圧
+N = 100; 
+Fp = 10e3; 
+Qp = 4e3; 
+Rp = 0.00057565; 
+Rst = 1e-4; 
 Fs = 44100;
-eqnum1 = firceqrip(N,Fp/(Fs/2),[Rp Rst],'high');%HPF
-eqnum2 = firceqrip(N,Qp/(Fs/2),[Rp Rst],'high');%HPF
-eqnum12 = eqnum1 - eqnum2;
+eqnuma = firceqrip(N,Fp/(Fs/2),[Rp Rst],'high');
+eqnumb = firceqrip(N,Qp/(Fs/2),[Rp Rst],'high');
+eqnumab = eqnuma - eqnumb;
 lowpassFIR = dsp.FIRFilter('Numerator', eqnum12);
 fvtool(lowpassFIR, 'Fs', Fs, 'Color', 'White');
-fileReader = dsp.AudioFileReader('music.mp3','SamplesPerFrame',frameLength); deviceWriter = audioDeviceWriter('SampleRate',fileReader.SampleRate);
+fileReader = dsp.AudioFileReader('music.mp3','SamplesPerFrame',frameLength);
+deviceWriter = audioDeviceWriter('SampleRate',fileReader.SampleRate);
 scope = dsp.SpectrumAnalyzer('SampleRate', fileReader.SampleRate);
 while ~isDone(fileReader)
 signal = fileReader();
