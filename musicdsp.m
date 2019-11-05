580830e2d0b723a634b664bf5de20bb067bfe331
fumi <fumi01200924@gmail.com>
2019-11-05 16:34:26 +0900

    musicDSP

--- musicdsp.m
+++ musicdsp.m
@@ -1,35 +1,22 @@
 frameLength = 4096;
-N   = 100;        % FIR filter order
-Fp  = 1e3;       % 1 kHz passband-edge frequency
-Rp  = 0.00057565; % Corresponds to 0.01 dB peak-to-peak ripple
-Rst = 1e-4;       % Corresponds to 80 dB stopband attenuation
+N = 100; % filter tap数
+Fp = 10e3; %10kHz passband
+Qp = 4e3; %4kHz passband
+Rp = 0.00057565; % パスバンドリップル
+Rst = 1e-4; % stopバンド抑圧
 Fs = 44100;
-% FIR filter design
-eqnum = firceqrip(N,Fp/(Fs/2),[Rp Rst],'passedge'); % eqnum = vec of coeffs
-%eqnum1 = firceqrip(N,Fp/(Fs/2),[Rp Rst],'high');%HPF
-%eqnum2 = firceqrip(N,Qp/(Fs/2),[Rp Rst],'high');%HPF
-%eqnum12 = eqnum1 - eqnum2;
-% passband edge is normalized with sampling frequency
-% create a filter
-lowpassFIR = dsp.FIRFilter('Numerator', eqnum);
-% show the characteristics of the filter
+eqnum1 = firceqrip(N,Fp/(Fs/2),[Rp Rst],'high');%HPF
+eqnum2 = firceqrip(N,Qp/(Fs/2),[Rp Rst],'high');%HPF
+eqnum12 = eqnum1 - eqnum2;
+lowpassFIR = dsp.FIRFilter('Numerator', eqnum12);
 fvtool(lowpassFIR, 'Fs', Fs, 'Color', 'White');
-% specify an audio file
-fileReader = dsp.AudioFileReader(...
-    'music.mp3',...
-    'SamplesPerFrame',frameLength);
-deviceWriter = audioDeviceWriter(...
-    'SampleRate',fileReader.SampleRate);
-scope = dsp.SpectrumAnalyzer(SampleRate', fileReader.SampleRate);
-while  ~isDone(fileReader)
-       % acquire frame lenth audio stream
-    signal = fileReader();
-       % apply LPF
-    yy = lowpassFIR(signal);
-       % write to speaker
+fileReader = dsp.AudioFileReader('music.mp3','SamplesPerFrame',frameLength); deviceWriter = audioDeviceWriter('SampleRate',fileReader.SampleRate);
+scope = dsp.SpectrumAnalyzer('SampleRate', fileReader.SampleRate);
+while ~isDone(fileReader)
+signal = fileReader();
+yy = lowpassFIR(signal);
     deviceWriter(yy);
-       % show
-    scope([signal,yy]);
+scope([signal,yy]);
 end
 release(fileReader);
 release(deviceWriter);
