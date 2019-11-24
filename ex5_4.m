9b775047ca07d197fc24a0c3ded7a67b232dd7e8
fumi <fumi01200924@gmail.com>
2019-11-24 14:00:03 +0900

    Update ex5_4.m

--- ex5_4.m
+++ ex5_4.m
@@ -7,10 +7,10 @@ dep=uint32(dep);
 
 array=zeros(col, row, dep);
 
-for d=0:1:dep-1
-    for i=0:1:col-1
-        for j=0:1:row-1
-            array(i, j, d) = (I(row*2, col*2, d) + I(row*2+1, col*2, d) + I(row*2, col*2+1, d) + I(row*2+1, col*2+1, d))/4;
+for d=0:dep-1
+    for i=0:col-1
+        for j=0:row-1
+            array(i, j, d) = (I(j*2, i*2, d) + I(j*2+1, i*2, d) + I(j*2, i*2+1, d) + I(j*2+1, i*2+1, d))/4;
         end
     end
 end
