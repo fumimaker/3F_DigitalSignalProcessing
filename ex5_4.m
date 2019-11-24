bfb699871aa1e240fde588c7e3e47787c27ded45
fumi <fumi01200924@gmail.com>
2019-11-24 14:49:18 +0900

    Update ex5_4.m

--- ex5_4.m
+++ ex5_4.m
@@ -5,16 +5,15 @@ image(I);
 row=uint32(row/2);
 col=uint32(col/2);
 dep=uint32(dep);
-array2=zeros(4,4,2)
 array=zeros(col, row, dep);
 
-for d=0:dep-1
-    for i=0:col-1
-        for j=0:row-1
-            upup=I(j*2, i*2, d);
-            updown=I(j*2-1, i*2, d);
-            downup=I(j*2, i*2-1, d);
-            downdown=I(j*2-1, i*2-1, d);
+for d=1:dep
+    for i=1:col
+        for j=1:row
+            upup=I(i*2, j*2, d);
+            updown=I(i*2-1, j*2, d);
+            downup=I(i*2, j*2-1, d);
+            downdown=I(i*2-1, j*2-1, d);
             array(i, j, d) = (upup+updown+downup+downdown)/4;
         end
     end
