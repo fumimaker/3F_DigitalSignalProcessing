eea4938f60815602de10ca817d8c6698c43baf2e
fumi <fumi01200924@gmail.com>
2019-11-24 15:01:27 +0900

    commit

--- ex5_4.m
+++ ex5_4.m
@@ -1,20 +1,21 @@
 I = imread('dachshund.jpg');
 whos I;
-image(I);
+
 [col, row, dep] = size(I);
-row=uint32(row/2);
-col=uint32(col/2);
+row=uint32(row/32);
+col=uint32(col/32);
 dep=uint32(dep);
 array=zeros(col, row, dep);
 
 for d=1:dep
     for i=1:col
         for j=1:row
-            upup=I(i*2, j*2, d);
-            updown=I(i*2-1, j*2, d);
-            downup=I(i*2, j*2-1, d);
-            downdown=I(i*2-1, j*2-1, d);
-            array(i, j, d) = (upup+updown+downup+downdown)/4;
+            upup = uint32(I(i*2, j*2, d));
+            updown = uint32(I(i*2-1, j*2, d));
+            downup = uint32(I(i*2, j*2-1, d));
+            downdown = uint32(I(i*2-1, j*2-1, d));
+            avg = uint32((upup+updown+downup+downdown)/4);
+            array(i, j, d) = avg;
         end
     end
 end
