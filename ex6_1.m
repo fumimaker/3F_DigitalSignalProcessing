314485c6e8d4224fb6a7ed0338a3c50996226634
fumi <fumi01200924@gmail.com>
2019-12-17 10:41:10 +0900

    Update ex6_1.m

--- ex6_1.m
+++ ex6_1.m
@@ -1,5 +1,3 @@
-
-
 % example of voice record
 recObj = audiorecorder(16000, 16, 1); % create a recorder object with sampling rate 16000 Hz
 disp("Start speaking"); % print out a start message
@@ -10,3 +8,21 @@ play(recObj); % play back the voice
 y = getaudiodata(recObj);
 plot(y) % visualize the array
 audiowrite('voice.wav', y, 16000); % save the audio data as a wave file. 
+
+%%  modulate5k440.m
+%    a 440 Hz tone is modulated
+%    onto 5kHz carrier
+
+Fs = 16000; % sampling rate
+fx = 440; % baseband signal freq.
+Fc = 5000; %carrier frequency
+Len = 2; % 2 seconds duration
+t = (0:1/Fs:Len-1/Fs); % from 0 to 2 second
+x = (1+sin(2*pi*fx*t)); %baseband signal (positive)
+f = (1/Len:1/Len:Fs); % frquency components
+y = x .* cos(2*pi*Fc*t); %superpose (modulation)
+sound(y, Fs);
+plot(f, abs(fft(y)));
+figure;
+plot(t(1:100), y(1:100));
+audiowrite('test.wav', y, Fs); %save the sound file
