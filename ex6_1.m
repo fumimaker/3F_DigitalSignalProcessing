bb73f48b9538961ea8c1cea1804fdfe11d763557
fumi <fumi01200924@gmail.com>
2019-12-17 10:54:19 +0900

    update

--- ex6_1.m
+++ ex6_1.m
@@ -11,16 +11,17 @@ audiowrite('voice.wav', y, 16000); % save the audio data as a wave file.
 disp("modulate");
 
 %%  modulate5k440.m
+
 [sample] = audioread('voice.wav');
 Fs = 16000; % sampling rate
 fx = 440; % baseband signal freq.
 Fc = 5000; %carrier frequency
 Len = 5; % 2 seconds duration
 t = (0:1/Fs:Len-1/Fs); % from 0 to 2 second
-%x = (1+sin(2*pi*fx*t)); %baseband signal (positive)
+x = (1+sin(2*pi*fx*t)); %baseband signal (positive)
 f = (1/Len:1/Len:Fs); % frquency components
-%y = x .* cos(2*pi*Fc*t); %superpose (modulation)
-y = sample .* cos(2*pi*Fc*t);
+y = x .* cos(2*pi*Fc*t); %superpose (modulation)
+%y = sample .* cos(2*pi*Fc*t);
 sound(y, Fs);
 plot(f, abs(fft(y)));
 figure;
