f414f64f67c8c84add4ac149c8757ccb5c8027ca
fumi <fumi01200924@gmail.com>
2020-01-05 15:04:06 +0900

    わからん

--- ex6_4.m
+++ ex6_4.m
@@ -8,7 +8,7 @@ dev = 0.2;  %adjust sigma
 vrr = resample(v, d, n);
 [r, c] = size(vrr);
 t = (0:1/Fs:r/Fs-1/Fs);
-y = zeros(r,2); % IQ signal storage
+y = zeros(r,1); % IQ signal storage
 for i=1:r
     y(i,1) = cos(2*pi()*(Fc+vrr(i)*dev)*t(i));
     y(i,2) = sin(2*pi()*(Fc+vrr(i)*dev)*t(i));
@@ -20,6 +20,9 @@ subplot(2,1,2)
 plot(vrr)
 title('baseband signal')
 
+maru = zeros(r,1);
+for i=1:r
+    maru(i,1) = atan(y(i,2)/y(i,1));
+end
 
-
-audiowrite('seyana5k20kfm.wav', y, Fs);
+audiowrite('seyana5k20kfm.wav', maru, Fs);
