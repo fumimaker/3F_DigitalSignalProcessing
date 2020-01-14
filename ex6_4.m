43e32e363123106db44fbf2a9b7d23b64564647a
fumi <fumi01200924@gmail.com>
2020-01-14 13:27:59 +0900

    add

--- ex6_4.m
+++ ex6_4.m
@@ -2,7 +2,7 @@
 %   水野史暁
 %   わかりませんでした...atan使って角度出せばいけるかなと思いました..
 Fc = 100000; %carrier frequency
-Fs = 44100;
+Fs = 300000;
 dev = 0.01;  %adjust sigma
 [v, Fvs] = audioread('seyanaTrim.wav');
 [n, d] = rat(Fvs/Fs); % 有理数近似(音源サンプリングレート/やりたいサンプリングレート)
@@ -24,11 +24,11 @@ audiowrite('seyana5k20kfm.wav', y, Fs);
 
 
 Fc = 100000;
-Fs = 44100;
+Fs = 300000;
 dev = 0.01; %adjust sigma
 [y, Fss] = audioread('seyana5k20kfm.wav');
 [r, c] = size(y);
-[n, d] = rat(Fss/Fs);
+[n, d] = rat(Fvs/Fs);
 x = zeros(r,1);
 for i=1:r-1
     x(i+1)=x(i)+(acos((y(i+1,1)*y(i,1)+y(i+1,2)*y(i,2))...
@@ -37,5 +37,5 @@ end
 yy=resample(x,d,n);
 %sound(yy,Fs);
 plot(yy);
-
-audiowrite('seyanaTrimDemodulated.wav', yy, Fs);
+sound(yy,Fvs);
+%audiowrite('seyanaTrimDemodulated.wav', yy, Fs);
--- fmRadio.m
+++ fmRadio.m
@@ -2,9 +2,10 @@
 %   êÖñÏéjã≈
 
 Fc = 75000;
-Fs = 240000;
-dev = 0.2; %adjust sigma
-[y, Fss] = audioread('../fameiq.wav');
+Fs = 200000;
+dev = 1; %adjust sigma
+[y, Fss] = audioread('fameiq.wav');
+y = real(y);
 [r, c] = size(y);
 [n, d] = rat(Fss/Fs);
 x = zeros(r,1);
@@ -13,7 +14,8 @@ for i=1:r-1
         /(sqrt(y(i+1,1)^2+y(i+1,2)^2)*sqrt(y(i,1)^2+y(i,2)^2)))-2*pi()*Fc/Fss)/dev;
 end
 yy=resample(x,d,n);
+yy=real(yy);
 %sound(yy,Fs);
 plot(yy);
 
-audiowrite('../fmradio.wav', yy, Fs);
+audiowrite('fmradioDemod.wav', yy, Fs);
